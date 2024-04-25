4a0ac56568800f9851cb9105726faa3a2bd4b4ed
amangujjar66 <166032720+amangujjar66@users.noreply.github.com>
2024-04-25 05:51:16 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//importing inquirer
+import inquirer from "inquirer";
+// taking input and storing it in variable answer 
+const answers = await inquirer.prompt([
+    {
+        type: "input",
+        name: "sentence",
+        message: "Enter a sentence:",
+    }
+]);
+// removing whitespaces
+// spliting tha sentance inti words and storing them in arry
+const words = answers.sentence.trim().split(" ");
+//printing tha array of words 
+console.log(words);
+//printing tha length of tha array of words 
+console.log(`Total Words count =${words.length}`);
